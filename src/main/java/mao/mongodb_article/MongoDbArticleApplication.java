15d8da8909944339c70d5e6b1f18105f6f6c36af
mao <1296193245@qq.com>
2022-11-16 15:04:48 +0800

    MongoDB_article

---
+++ src/main/java/mao/mongodb_article/MongoDbArticleApplication.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbArticleApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MongoDbArticleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/mongodb_article/MongoDbArticleApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbArticleApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
